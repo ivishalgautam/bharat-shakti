b08481f509299b8e6ebfb1cdf049d55c3f4f2290
Vishal Gautam <ivishalgautamm@gmail.com>
2025-04-08 10:29:06 +0530

    bug fiixes

--- client/web/src/config/index.js
+++ client/web/src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
-  api_base: process.env.NEXT_PUBLIC_API_URL,
-  file_base: process.env.NEXT_PUBLIC_IMAGE_DOMAIN,
+  api_base: process.env.NEXT_PUBLIC_BHARAT_SHAKTI_API_URL,
+  file_base: process.env.NEXT_PUBLIC_BHARAT_SHAKTI_FILE_BASE,
 };
 
 export default config;
