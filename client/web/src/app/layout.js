3fc7fd0277fcab9fbeb4a682574b021dc5da3b63
Vishal Gautam <ivishalgautamm@gmail.com>
2025-06-10 13:25:54 +0530

    change in meta details

--- client/web/src/app/layout.js
+++ client/web/src/app/layout.js
@@ -16,8 +16,17 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "Bharat Shakti",
-  description: "Bharat Shakti",
+  title:
+    "BharatShaktiTenders.com – India’s #1 Portal for Government & PSU Tenders",
+  description:
+    "Discover the latest 2025 government tenders from Central & State departments across India. Bid online for GeM, PSU, infrastructure, and defense projects. Get free tender alerts via SMS & Email. Stay ahead with real-time updates and seamless e-bidding on BharatShaktiTenders.com – your trusted gateway to public procurement opportunities.",
+  keywords:
+    "government tenders 2025, Indian tenders, e tender India, GeM tenders, PSU tenders, infrastructure tenders, state government tenders, central government tenders, online tender bidding, Bharat Shakti Tenders, tender alerts India, free tender updates, public procurement India",
+  openGraph: {
+    title: "Latest Indian Government Tenders 2025 | BharatShaktiTenders.com",
+    description:
+      "Access the latest 2025 tenders from Central & State Governments, PSUs, GeM, and infrastructure projects. Register on BharatShaktiTenders.com to get free SMS/Email alerts and bid online with ease.",
+  },
 };
 
 export default function RootLayout({ children }) {
