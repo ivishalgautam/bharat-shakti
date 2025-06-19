46591563ff9aae49335d7776743aec675d26006e
Vishal Gautam <ivishalgautamm@gmail.com>
2025-06-19 16:20:49 +0530

    bug fixes

--- client/web/src/components/logo.jsx
+++ client/web/src/components/logo.jsx
@@ -4,19 +4,22 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <figure className="flex aspect-video w-[130px] items-center md:w-[180px]">
+      <div className="relative w-[130px] md:w-[180px]">
         <Image
           src="/logo.png"
           width={180}
           height={101}
           alt="Bharat Shakti"
-          className="rounded object-contain object-center"
+          className="rounded object-contain"
           priority
           sizes="(max-width: 768px) 130px, 180px"
-          placeholder="blur"
-          blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
+          style={{
+            width: "100%",
+            height: "auto",
+            maxWidth: "100%",
+          }}
         />
-      </figure>
+      </div>
     </Link>
   );
 }
