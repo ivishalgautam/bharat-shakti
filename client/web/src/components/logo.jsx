034c02a7807e38516bcf9e7bb8f1a064486c57ca
Vishal Gautam <ivishalgautamm@gmail.com>
2025-06-19 16:16:59 +0530

    bug fixes

--- client/web/src/components/logo.jsx
+++ client/web/src/components/logo.jsx
@@ -6,13 +6,15 @@ export default function Logo() {
     <Link href="/" className="flex items-center gap-2">
       <figure className="flex aspect-video w-[130px] items-center md:w-[180px]">
         <Image
-          src={"/logo.png"}
-          width={500}
-          height={500}
+          src="/logo.png"
+          width={180}
+          height={101}
           alt="Bharat Shakti"
           className="rounded object-contain object-center"
-          quality={100}
-          loading="lazy"
+          priority
+          sizes="(max-width: 768px) 130px, 180px"
+          placeholder="blur"
+          blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
         />
       </figure>
     </Link>
