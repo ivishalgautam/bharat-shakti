3fb9fe3593c511a883c8e3cfe9756d5b45dd9418
Vishal Gautam <ivishalgautamm@gmail.com>
2025-07-01 19:05:06 +0530

    ui changes

--- client/web/src/app/(auth)/login/page.jsx
+++ client/web/src/app/(auth)/login/page.jsx
@@ -1,11 +1,12 @@
 import LoginForm from "@/components/forms/login";
+import { publicRoutes } from "@/data/routes";
 import { headers } from "next/headers";
 import React from "react";
 
 export default function LoginPage() {
   const headerList = headers();
   const referer = headerList.get("referer");
-  const redirectLink = referer.includes("/register") ? "/" : referer;
+  const redirectLink = publicRoutes.includes(referer) ? "/" : referer;
 
   return <LoginForm redirectLink={redirectLink} />;
 }
